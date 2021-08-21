13a287fed4536400ca933064dfc0082748b18d14
crazhou <crazhou@163.com>
2021-08-22 01:03:37 +0800

    有些文件使用了 gbk 编码

--- hardward/max7219/max7219.c
+++ hardward/max7219/max7219.c
@@ -5,58 +5,53 @@
  */
 void Max7219_init(void)
 {
-	GPIO_InitTypeDef GPIO_InitStructure;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_InitStructure.GPIO_Pin = MAX7219_CS_PIN|MAX7219_CLK_PIN|MAX7219_DATA_PIN;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(MAX7219PORT, &GPIO_InitStructure);
-	
-	
-	// 时钟和数据线拉低
-	GPIO_ResetBits(MAX7219PORT,MAX7219_CLK_PIN|MAX7219_DATA_PIN);
-	
-	// 片选接口拉高
-	GPIO_SetBits(MAX7219PORT,MAX7219_CS_PIN);
-	
-	Max7219_decode_mode(DECODE_ALL);
-	Max7219_intensity(0x04);
-	Max7219_scan_limit(0x07);
-	Max7219_shutdown(NORMAL_MODE);
+  GPIO_InitTypeDef GPIO_InitStructure;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+  GPIO_InitStructure.GPIO_Pin = MAX7219_CS_PIN | MAX7219_CLK_PIN | MAX7219_DATA_PIN;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(MAX7219PORT, &GPIO_InitStructure);
 
-		
-	// 开始测试
-	Max7219_test(0x01);	
- 	delay_ms(1000);
-	Max7219_test(0x00);
+  // 时钟和数据线拉低
+  GPIO_ResetBits(MAX7219PORT, MAX7219_CLK_PIN | MAX7219_DATA_PIN);
 
+  // 片选接口拉高
+  GPIO_SetBits(MAX7219PORT, MAX7219_CS_PIN);
+
+  Max7219_decode_mode(DECODE_ALL);
+  Max7219_intensity(0x04);
+  Max7219_scan_limit(0x07);
+  Max7219_shutdown(NORMAL_MODE);
+
+  // 开始测试
+  Max7219_test(0x01);
+  delay_ms(1000);
+  Max7219_test(0x00);
 }
-
 
 void Max7219_Send(uint8_t addr, uint8_t data)
 {
-	int8_t i;
-	uint8_t tmp;
-	
-	GPIO_ResetBits(MAX7219PORT,MAX7219_CS_PIN);
-	
-	for (i = 7; i >= 0; i--)
-	{
-		tmp = (addr>>i) & 0x01;
-		GPIO_WriteBit(MAX7219PORT, MAX7219_DATA_PIN, (BitAction)tmp);
-		GPIO_SetBits(MAX7219PORT,MAX7219_CLK_PIN);
-		GPIO_ResetBits(MAX7219PORT,MAX7219_CLK_PIN);
-	}
-	
-	for (i = 7; i >= 0; i--)
-	{
-		tmp = (data>>i) & 0x01;
-		GPIO_WriteBit(MAX7219PORT, MAX7219_DATA_PIN, (BitAction)tmp);
-		GPIO_SetBits(MAX7219PORT,MAX7219_CLK_PIN);
-		GPIO_ResetBits(MAX7219PORT,MAX7219_CLK_PIN);
-	}
-	
-	GPIO_SetBits(MAX7219PORT,MAX7219_CS_PIN);
+  int8_t i;
+  uint8_t tmp;
 
+  GPIO_ResetBits(MAX7219PORT, MAX7219_CS_PIN);
+
+  for (i = 7; i >= 0; i--)
+  {
+    tmp = (addr >> i) & 0x01;
+    GPIO_WriteBit(MAX7219PORT, MAX7219_DATA_PIN, (BitAction)tmp);
+    GPIO_SetBits(MAX7219PORT, MAX7219_CLK_PIN);
+    GPIO_ResetBits(MAX7219PORT, MAX7219_CLK_PIN);
+  }
+
+  for (i = 7; i >= 0; i--)
+  {
+    tmp = (data >> i) & 0x01;
+    GPIO_WriteBit(MAX7219PORT, MAX7219_DATA_PIN, (BitAction)tmp);
+    GPIO_SetBits(MAX7219PORT, MAX7219_CLK_PIN);
+    GPIO_ResetBits(MAX7219PORT, MAX7219_CLK_PIN);
+  }
+
+  GPIO_SetBits(MAX7219PORT, MAX7219_CS_PIN);
 }
 
 /*
@@ -64,9 +59,9 @@ void Max7219_Send(uint8_t addr, uint8_t data)
  */
 int Max7219_shutdown(uint8_t mode)
 {
-	Max7219_Send(SHUT_DOWN, mode);
-	
-	return 0;
+  Max7219_Send(SHUT_DOWN, mode);
+
+  return 0;
 }
 
 /*
@@ -79,10 +74,10 @@ int Max7219_shutdown(uint8_t mode)
  */
 int Max7219_decode_mode(uint8_t mode)
 {
-	
-	Max7219_Send(DECODE_MODE, mode);
-	
-	return 0;
+
+  Max7219_Send(DECODE_MODE, mode);
+
+  return 0;
 }
 
 /* 
@@ -91,10 +86,10 @@ int Max7219_decode_mode(uint8_t mode)
  */
 int Max7219_intensity(uint8_t mode)
 {
-	
-	Max7219_Send(INTENSITY, mode);
-	
-	return 0;
+
+  Max7219_Send(INTENSITY, mode);
+
+  return 0;
 }
 
 /* 
@@ -103,10 +98,10 @@ int Max7219_intensity(uint8_t mode)
  */
 int Max7219_scan_limit(uint8_t mode)
 {
-	
-	Max7219_Send(SCAN_LIMIT, mode);
-	
-	return 0;
+
+  Max7219_Send(SCAN_LIMIT, mode);
+
+  return 0;
 }
 
 /* 
@@ -115,42 +110,44 @@ int Max7219_scan_limit(uint8_t mode)
  */
 int Max7219_test(uint8_t mode)
 {
-	
-	Max7219_Send(DISPLAY_TEST, mode);
-	
-	return 0;
+
+  Max7219_Send(DISPLAY_TEST, mode);
+
+  return 0;
 }
 
 int8_t indexOf(char *str, uint8_t ch)
 {
-	int8_t index = 0;
-	
-	char tmp = str[index];
-	
-	while(tmp != '\0') {
-		
-		if(tmp == ch) {
-			return index;
-		}
-		index++;
-		tmp = str[index];
-	}
-	
-	return -1;
+  int8_t index = 0;
+
+  char tmp = str[index];
+
+  while (tmp != '\0')
+  {
+
+    if (tmp == ch)
+    {
+      return index;
+    }
+    index++;
+    tmp = str[index];
+  }
+
+  return -1;
 }
 
 int Max7219_digit(uint8_t digit, uint8_t data)
 {
-	int8_t tmp = 0;
-	
-	char str[] = "0123456789-EHLP ";
-	
-	tmp = indexOf(str, data);
-	
-	if(tmp >= 0) {
+  int8_t tmp = 0;
 
-		Max7219_Send(digit, (uint8_t)tmp);
-	}
-	return 0;
+  char str[] = "0123456789-EHLP ";
+
+  tmp = indexOf(str, data);
+
+  if (tmp >= 0)
+  {
+
+    Max7219_Send(digit, (uint8_t)tmp);
+  }
+  return 0;
 }
-
--- hardward/max7219/max7219.h
+++ hardward/max7219/max7219.h
@@ -12,24 +12,23 @@
 #define MAX7219_CS_PIN GPIO_Pin_3
 #define MAX7219_DATA_PIN GPIO_Pin_4
 
-
 /*  地址定义 */
-#define  DECODE_MODE  0x09   // 选择显示模式
-#define  INTENSITY  0x0a   // 控制亮度
-#define  SCAN_LIMIT  0x0b   // 扫描限制 
-#define  SHUT_DOWN  0x0c   // 进入低功耗
-#define  DISPLAY_TEST  0x0f   // 显示测试
+#define DECODE_MODE 0x09  // 选择显示模式
+#define INTENSITY 0x0a    // 控制亮度
+#define SCAN_LIMIT 0x0b   // 扫描限制
+#define SHUT_DOWN 0x0c    // 进入低功耗
+#define DISPLAY_TEST 0x0f // 显示测试
 
 /* mode define */
-#define NORMAL_MODE      1
-#define SHUTDOWN_MODE    0
+#define NORMAL_MODE 1
+#define SHUTDOWN_MODE 0
 
-#define NO_DECODE_ALL   0x00
-#define DECODE_D0_ONLY   0x01
+#define NO_DECODE_ALL 0x00
+#define DECODE_D0_ONLY 0x01
 #define DECODE_D0D3_ONLY 0x0f
-#define DECODE_ALL      0xff
+#define DECODE_ALL 0xff
 
-#define Segment_Line_DP (1<<7);
+#define Segment_Line_DP (1 << 7);
 
 void Max7219_init(void);
 int Max7219_digit(uint8_t digit, uint8_t data);
@@ -38,6 +37,5 @@ int Max7219_intensity(uint8_t mode);
 int Max7219_scan_limit(uint8_t mode);
 int Max7219_shutdown(uint8_t mode);
 int Max7219_test(uint8_t mode);
-
 
 #endif
